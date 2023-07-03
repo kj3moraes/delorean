15b451d8efdba8d2e7c9e29cc8e09493fb892346
Keane Moraes <lordvader3002@gmail.com>
2023-07-03 07:40:01 -0400

    expansion returns a list of tuples

--- pydelorean/tools/__init__.py
+++ pydelorean/tools/__init__.py
@@ -1,7 +1,6 @@
-from treelib import Tree, Node
-from pydelorean.tree.types import TextNode, HeaderNode
+from pydelorean.tree.types import HeaderNode
 
-def get_progressive_expansion(tree:Tree, node_id, **kwargs) -> list:
+def get_progressive_expansion(tree, node_id, **kwargs) -> list:
     
     node = tree.get_node(node_id)
     
@@ -12,17 +11,16 @@ def get_progressive_expansion(tree:Tree, node_id, **kwargs) -> list:
     header = node.data.header
     child_ids = tree.children(node_id)
     returned_list = []
-    final_corpus = header
+    final_corpus = ""
     for child_id in child_ids:
         child_corpus = get_progressive_expansion(tree, child_id.identifier)
         print("\n===THE CHILD CORPUS IS\n", child_corpus)
         for child in child_corpus:
-            returned_list.append(header + "\n" + child)
+            returned_list.append((header,   child))
             final_corpus += "\n" + child
     
     if 'append' in kwargs and kwargs['append']:
-        returned_list.append(final_corpus)
-    # print("===THE FINAL CORPUS IS\n", final_corpus)
+        returned_list.append((header, final_corpus))
     return returned_list
         
     
